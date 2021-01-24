9f7cf6adcc170f3b30cc21c18977004ab89012e2
kuga2 <kuga2@users.noreply.github.com>
2021-01-24 12:15:44 +0100

    Change Pi.GPIO to gpiozero
    because it has mock factory for desktiop development.
    Use: export GPIOZERO_PIN_FACTORY=mock

--- led.py
+++ led.py
@@ -1,20 +1,21 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED
 
 class Led():
-    _pin = 0
+    led = None
+    pin = 0
     def __init__(self, pin):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        if(pin is not None):
-            self._pin = pin
-        GPIO.setup(self._pin, GPIO.OUT)
+        self.pin = pin
+        self.led = LED(pin)
 
     def status(self):
-        return GPIO.input(self._pin)
+        if self.leds_on():
+            return 1
+        else:
+            return 0
     def leds_on(self):
-        return self.status() == 1
+        return self.led.is_active
     def switch_leds(self, on):
         if(on):
-            GPIO.output(self._pin, GPIO.HIGH)
+            self.led.on()
         else:
-            GPIO.output(self._pin, GPIO.LOW)
+            self.led.off()
